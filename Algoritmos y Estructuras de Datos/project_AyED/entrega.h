4b972d9e36a0ecbab82f66c43bc5efb08c70fb4f
Grance Zenteno Lucas Rodrigo <lucasrgz2025@gmail.com>
2026-03-11 16:56:37 -0300

    Delete Algoritmos y Estructuras de Datos directory
    borrado del proyecto de algoritmos

--- Algoritmos y Estructuras de Datos/project_AyED/entrega.h
+++
@@ -1,6 +0,0 @@
-#ifndef ENTREGA_H_INCLUDED
-#define ENTREGA_H_INCLUDED
-
-int ejercicioMenu();
-
-#endif // ENTREGA_H_INCLUDED
--- Algoritmos y Estructuras de Datos/project_AyED/miscelaneos.h
+++
@@ -1,6 +0,0 @@
-#ifndef MISCELANEOS_H_INCLUDED
-#define MISCELANEOS_H_INCLUDED
-
-void MostrarEntero(void *info);
-int MayorEntero(const void *a, const void *b);
-#endif // MISCELANEOS_H_INCLUDED
